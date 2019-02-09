a0ed386ae45ce56f262804b133bc55504d67ffd0
LeoniePeters <leoniepeters@gmail.com>
2019-02-09 13:58:48 +0100

    create get all /playlists

--- playlists/routes.js
+++ playlists/routes.js
@@ -17,4 +17,13 @@ router.post('/playlists', (req, res, next) => {
     .catch(error => next(error))
 })
 
+router.get('/playlists', (req, res, next) => {
+    Playlist
+      .findAll()
+      .then(playlists => {
+        res.send({ playlists })
+      })
+      .catch(error => next(error))
+  })
+
 module.exports = router
