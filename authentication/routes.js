bd017194e9f74b65b105b0c5511f6977b3345043
LeoniePeters <leoniepeters@gmail.com>
2019-02-09 11:54:53 +0100

    create sign in

---
+++ authentication/routes.js
@@ -0,0 +1,46 @@
+const {Router} = require('express')
+const {toJWT, toData} = require('./jwt')
+const User = require('../users/model')
+const router = new Router()
+const bcrypt = require('bcrypt')
+
+router.post('/tokens', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  
+  if (!email || !password) {
+    res.status(404).send({
+      message: 'Please try again'
+    })
+  } else {
+    User
+      .findOne({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(someUser => {
+        if (!someUser) {
+          return res.status(404).send({
+            message: 'Please try again'
+          })
+        } else if (bcrypt.compareSync(req.body.password, someUser.password)) {
+          res.send({
+            token: toJWT({ userId: someUser.id })
+          })
+        } else {
+          res.status(404).send({
+            message: 'Please try again'
+          })
+        }
+      })
+      .catch(err => {
+        console.error(err)
+        res.status(500).send({
+          message: 'Something went wrong'
+        })
+      })
+  }
+})
+
+module.exports = router
