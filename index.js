665e5a1c6e14ef5ffed82003e17676477e5304e2
LeoniePeters <leoniepeters@gmail.com>
2019-02-09 10:22:58 +0100

    New project

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+
+//use this later in model.js
+// const Sequelize = require('sequelize')
+// const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres', {define: { timestamps: false }})
